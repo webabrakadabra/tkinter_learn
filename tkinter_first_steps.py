5048799e9b7d52ab022e334e97f98754e4b6fcfd
grey <hireshnik@gmail.com>
2021-07-08 15:58:07 +0300

    add isert

--- tkinter_first_steps.py
+++ tkinter_first_steps.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+from tkinter import filedialog
 from tkinter import *
 import pyautogui
 
 def insert():
 	pyautogui.hotkey('ctrl', 'v')
+	
+def browse_button():
+    filename = filedialog.askdirectory()
+    txt2.delete(0, 'end')
+    txt2.insert(0, filename)
+    # ~ print(filename)
+    # ~ return filename
 	
 window = Tk()
 window.title("Текст0")
@@ -16,12 +24,13 @@ lb2.grid(column=0, row=2, padx=(10, 0))
 
 btn1 = Button(window, text="Завантажити", width=40)
 btn2 = Button(window, text="Вставити", command=insert)
-btn3 = Button(window, text="Вибрати")
+btn3 = Button(window, text="Вибрати", command=browse_button)
 btn1.grid(column=0, row=4)
 btn2.grid(column=2, row=1)
 btn3.grid(column=2, row=3)
 
 txt1 = Entry(window, width=51)
+
 txt2 = Entry(window, width=51)
 txt1.grid(column=0, row=1, padx=(10, 10))
 txt2.grid(column=0, row=3, padx=(10, 10), pady=(0, 10))
