2156ef0b2a12e9fb5d54f73f3ba80c899fef2733
webabrakadabra <hirehsnik@gmail.com>
2021-07-08 17:44:10 +0300

    aa

--- youtube.py
+++ youtube.py
@@ -9,7 +9,7 @@ try:
 	youtube = pytube.YouTube(url)
 	video = youtube.streams.first()
 	print('Кліп:', youtube.title, 'розміром', youtube.length, 'буде завантажено в /home/grey/Відео/clips')
-	video.download('/home/grey/Відео/clips')
+	video.download('E:/')
 	print('Кліп завантажено')
 except Exception:
 	print('Щось пішло не так......')
