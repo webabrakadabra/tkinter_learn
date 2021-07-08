8758f184989c76536ddfd1a702f5989d77e74e4a
grey <hireshnik@gmail.com>
2021-07-08 15:33:27 +0300

    add youtube.py

---
+++ youtube.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pytube
+
+
+url = input('ВВедіть адресу кліпу: \n')
+
+try:
+	youtube = pytube.YouTube(url)
+	video = youtube.streams.first()
+	print('Кліп:', youtube.title, 'розміром', youtube.length, 'буде завантажено в /home/grey/Відео/clips')
+	video.download('/home/grey/Відео/clips')
+	print('Кліп завантажено')
+except Exception:
+	print('Щось пішло не так......')
+	
+
+
