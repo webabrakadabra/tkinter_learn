81a5daef6ea41dee3d0494060702b94ae6e7fa9d
grey <hireshnik@gmail.com>
2021-07-13 16:21:20 +0300

    d

--- test0.py
+++ test0.py
@@ -1,4 +1,11 @@
 from pytube import YouTube
 
 yt = YouTube('https://www.youtube.com/watch?v=34Ey4i3wp-8')
-print(yt.streams)
+# ~ x = str(yt.streams[0]).split()
+lst = []
+
+for i in range(len(yt.streams)):
+	lst.append(str(yt.streams[i]).split())
+print(lst)
+
+
