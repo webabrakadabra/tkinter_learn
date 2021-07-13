02d4230eb56fccef72cc8208e4824b16acd061f5
grey <hireshnik@gmail.com>
2021-07-13 16:34:03 +0300

    d

--- test0.py
+++ test0.py
@@ -1,11 +1,11 @@
 from pytube import YouTube
 
 yt = YouTube('https://www.youtube.com/watch?v=34Ey4i3wp-8')
-# ~ x = str(yt.streams[0]).split()
-lst = []
+# ~ lst = []
 
-for i in range(len(yt.streams)):
-	lst.append(str(yt.streams[i]).split())
-print(lst)
+# ~ for i in range(len(yt.streams)):
+	# ~ lst.append(str(yt.streams[i]).split())
+# ~ print(lst)
+yt.streams.get_by_itag(140).download()
 
 
