0c7c913a263e2344feb6433fe1d492c8f47d88d7
grey <hireshnik@gmail.com>
2021-07-13 13:54:00 +0300

    f

--- tkinter_first_steps.py
+++ tkinter_first_steps.py
@@ -24,10 +24,13 @@ def download():
     x2 = txt2.get()
     try:
         youtube = pytube.YouTube(x)
-        video = youtube.streams.first()
+        video = youtube.streams.filter(res="720p").first()
         print('Кліп:', youtube.title, 'розміром', youtube.length, 'буде завантажено в /home/grey/Відео/clips')
+        lb3.config(text = "Завантаження:" + youtube.title)
+        # ~ window.update()
         video.download(x2)
         #window.update()
+        lb3.config(text = "Відео: " + youtube.title + " завантажено")
         print('Кліп завантажено')
         
     except Exception:
@@ -42,20 +45,22 @@ def starter():
 window = Tk()
 window.title("Програма загрузки роликів з YouTube")
 window.configure(background='bisque')
-window.geometry("700x200")
+# ~ window.geometry("700x200")
 lb1 = Label(window, text="Вставте скопійоване посилання на відео з Youtube:", font=("Times New Roman", 14))
 lb2 = Label(window, text="Виберіть директорію для збереження відео:", font=("Times New Roman", 14))
+lb3 = Label(window, font=("Times New Roman", 14))
 lb1.configure(background='bisque')
 lb2.configure(background='bisque')
 lb1.grid(column=0, row=0, padx=(10, 0))
 lb2.grid(column=0, row=2, padx=(10, 0))
+lb3.grid(column=0, row=5, padx=(10, 0), pady=(10, 10))
 
 btn1 = Button(window, text="Завантажити", width=40, command=starter)
 btn2 = Button(window, text="Вставити", command=insert)
 btn3 = Button(window, text="Вибрати", command=browse_button)
-btn1.grid(column=0, row=4)
-btn2.grid(column=2, row=1)
-btn3.grid(column=2, row=3)
+btn1.grid(column=0, row=4, padx=(10, 10))
+btn2.grid(column=2, row=1, padx=(10, 10))
+btn3.grid(column=2, row=3, padx=(10, 10))
 
 txt1 = Entry(window, width=70, bd=2)
 txt2 = Entry(window, width=70, bd=2)
@@ -63,7 +68,5 @@ txt1.grid(column=0, row=1, padx=(10, 10))
 txt2.grid(column=0, row=3, padx=(10, 10), pady=(0, 10))
 
 
+
 window.mainloop()
-
-
-
